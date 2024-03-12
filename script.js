243beae3533719d86574eba6aa327c5d7f39f723
Eric Fargo <fargo10k@gmail.com>
2024-03-12 11:32:47 -0400

    first function

---
+++ script.js
@@ -0,0 +1,29 @@
+// John Bass, Lillian Layne, other friends, and youtube 
+
+
+// cached items
+const board = document.getElementById('board');
+const score1 = document.getElementById('playerScores1');
+const score2 = document.getElementById('playerScores2');
+const score3 = document.getElementById('playerScores3');
+
+// varibles
+let snake = [{x: 10, y: 10 }];
+let highScores = []
+
+// functions
+
+// drawing everything on game bored
+draw(() => {
+    board.innerHTML = '';
+    drawSnake();
+})
+
+drawSnake(() =>{
+    snake.forEach((segment) => {
+        const snakeEl = createGameElement('div', 'snake');
+    });
+});
+
+
+
