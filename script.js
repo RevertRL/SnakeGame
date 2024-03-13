6c4a38c96275a02a7c03c877130bf084af912f41
Eric Fargo <fargo10k@gmail.com>
2024-03-12 22:26:30 -0400

    functions

--- script.js
+++ script.js
@@ -6,28 +6,84 @@ const board = document.getElementById('board');
 const score1 = document.getElementById('playerScores1');
 const score2 = document.getElementById('playerScores2');
 const score3 = document.getElementById('playerScores3');
+const gridSize = 20;
 
 // varibles
-let snake = [{x: 10, y: 10 }];
-let highScores = []
+let snake = [{ x: 10, y: 10 }];
+let food = foodSpawner()
+let direction = 'right';
+let highScores = [];
 
 // functions
 
 // drawing everything on game bored
-draw(() => {
+function draw() {
     board.innerHTML = '';
     drawSnake();
-})
+    drawFood();
+}
 
-drawSnake(() =>{
+function drawSnake() {
     snake.forEach((segment) => {
         const snakeEl = createGameElement('div', 'snake');
+        setPosition(snakeEl, segment);
+        board.appendChild(snakeEl);
     });
-});
+};
 
-createGameElement((tag, className) => {
+function createGameElement(tag, piece) {
     const gameEl = document.createElement(tag);
-})
+    gameEl.className = piece;
+    return gameEl;
+}
 
 
+function setPosition(gameEl, position) {
+    gameEl.style.gridColumn = position.x;
+    gameEl.style.gridRow = position.y;
+}
 
+draw();
+
+function drawFood() {
+        const foodEl = createGameElement('div', 'food');
+        setPosition(foodEl, food);
+        board.appendChild(foodEl);
+}
+
+function foodSpawner() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
+}
+
+// player movement
+
+function move() {
+    const head = {...snake[0] };
+    switch (direction) {
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+        case 'right':
+            head.x++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+    }
+
+    snake.unshift(head);
+
+    snake.pop();
+
+}
+
+
+// setInterval(() => {
+//     move();
+//     draw();
+// }, 200);
